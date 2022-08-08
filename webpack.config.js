70a8f318f7aa5a8b78903844d4362c287fa26e76
Alexey Yanvarev <yanvarev_2015@mail.ru>
2022-08-08 13:25:53 +0400

    feat: Setup environment

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCSSAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config;
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'build'),
+        libraryExport: 'default'
+    },
+    optimization: optimization(),
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    devServer: {
+        port: 5000,
+        hot: isDev
+    },
+    devtool: isDev ? 'source-map' : 'eval',
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css') //'[name].[contenthash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
